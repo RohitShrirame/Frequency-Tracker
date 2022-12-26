38ef19cc1c8533bed072ac599271b9f5d57c6b92
Rohit Shrirame <119651933+RohitShrirame@users.noreply.github.com>
2022-12-26 15:43:57 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+if( mpp.Search(tmp) )
+                //     cout<<"The word "<<tmp<<" is present in our paragraph/sentence "<<endl;
+                // else
+                //     cout<<"The word "<<tmp<<" is not present in our paragraph/sentence"<<endl;
